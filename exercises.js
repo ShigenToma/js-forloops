a789d9ad70459c6e41af76380d1706bcdd2a9817
unknown <shigntoma@gmail.com>
2018-06-13 12:55:28 -1000

    presidents

--- exercises.js
+++ exercises.js
@@ -22,6 +22,9 @@ This week's chart buster is: '24K Magic.'
 */
 
 var topFive = ["Closer", "Starboy", "I Feel It Coming", "Let Me Love You", "24K Magic"];
+for(var i = 0; i<topFive.length; i++){
+  console.log("This week's chart buster is: " + topFive[i]);
+}
 
 
 /* 3) Dead Presidents
@@ -43,6 +46,18 @@ This function will iterate through the person parameter and console.log the foll
 
 "President person was a great leader."
 */
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+for(var i = 0; i<presidents.length; i++){
+  console.log("The value at " + i + " is " + presidents[i]);
+}
+  console.log(presidents.length);
+
+  function leaders(person){
+    for(var i = 0; i<person.length; i++){
+    console.log("President " + person[i] + " was a great leader.");
+   }
+  }
+leaders(presidents);
 
 
 /* 4) Line Number
