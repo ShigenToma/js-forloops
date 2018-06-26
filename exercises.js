77432b2677067ba1168a30d04ed414e60308d352
unknown <shigntoma@gmail.com>
2018-06-26 10:55:01 -1000

    forloops

--- exercises.js
+++ exercises.js
@@ -64,7 +64,11 @@ leaders(presidents);
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
-
+var stringOfNumbers = "";
+for( var i = 10; i<21; i++){
+  stringOfNumbers += i;
+}
+console.log(stringOfNumbers);
 
 /* 5) Even Stevens
 Declare a variable named `evenNumArr` and assign its value to an empty array. 
@@ -72,7 +76,13 @@ Declare a variable named `evenNumArr` and assign its value to an empty array.
 Write a for-loop that will push even numbers to the `evenNumArr` array. We want to push 50 even even numbers starting from 0.
 Console.log your results.
 */
-
+var evenNumArr = [];
+for(var i = 0; i<100; i++){
+  if(i%2 === 0){
+    evenNumArr.push(i);
+  }
+}
+console.log(evenNumArr);
 
 /* 6) Up the Odds
 Declare a variable named `oddSum` and assign it to the Number value 0.
@@ -80,7 +90,13 @@ Declare a variable named `oddSum` and assign it to the Number value 0.
 Write a for-loop that will sum up odd numbers to the `oddSum` variable. We want to add 50 odd numbers starting from 1.
 Console.log your results.
 */
-  
+var oddSum = 0
+for(var i = 1; i<100; i++){
+  if(i%2 === 1){
+oddSum += i;
+  }
+}
+  console.log(oddSum);
 
 /* 7) Oops There It is
 Declare a variable named `oopsArray` and assign its to the following array: `[ 'turn' , , 'down' , , 'for' , , 'what' ]`.
@@ -88,7 +104,14 @@ Note that every odd index value in `oopsArray` is currently `undefined`. Using a
 
 [ 'turn' , 'nope' , 'down' , 'nope' , 'for' , 'nope' , 'what' ]
 */
-
+var oopsArray = [ 'turn' , , 'down' , , 'for' , , 'what' ];
+for(var i = 0; i<oopsArray.length; i++){
+  console.log(oopsArray[i]);
+  if(i%2 === 1){
+    oopsArray[i] = "nope";
+  }
+}
+console.log(oopsArray);
 
 /* 8) Is It There Oops
 Using a for-loop, iterate through the Array stored at `oopsArray` backwards. Console.log your result. It should look like this:
@@ -101,6 +124,9 @@ down
 nope
 turn
 */
+for(var i = oopsArray.length -1; i>0; i--){
+  console.log(oopsArray[i]);
+}
 
 
 /* 9) Siesta Time
@@ -112,6 +138,17 @@ Next, write a function named `nap`. This function takes in a single parameter: `
 
 Inside of this function write a for-loop that will iterate through the `napSchedule` array and console.log the message: `ZzZzZzZz` if the schedule is `true`, otherwise the it will console.log the message: `Gotta get coding!` if the schedule is `false`.
 */
+var napSchedule = [false, false, true, false, true, true];
+function nap(schedule){
+  for(var i = 0; i<napSchedule.length; i++){
+    if(schedule[i] === true){
+      console.log("ZzZzZzZz");
+    }else{
+      console.log("Gotta get coding!");  
+    }
+  }
+}
+nap(napSchedule);
 
 
 
@@ -126,6 +163,13 @@ Write a function named `copyArray` which takes two arguments: `originArray` and 
 
 Inside of this function write a for-loop that will iterate through the contents of the `originArray` and pushes each element of that array into `destinationArray`. Console.log your result.
 */
+var valuesArray = [99, 66, 829, 1941, 8, 76];
+var copyvaluesArray = [];
+function copyArray(originArray, destinationArray){
+  for(var i = 0; i<originArray.length; i++){
+
+  }
+}
 
 
 
